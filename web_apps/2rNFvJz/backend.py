17fb82da2ebe6380772ceff1d8c469b381c4bc09
admin <nicolas.rouyer@neo4j.com>
2022-04-28 21:51:48 +0000

    Saved web app the wine app (TRYDSS.2rNFvJz)

--- web_apps/2rNFvJz/backend.py
+++ web_apps/2rNFvJz/backend.py
@@ -1,33 +1,33 @@
 from math import pi
-from bokeh.io import curdoc
-from bokeh.layouts import row, widgetbox
-from bokeh.models import BasicTicker, ColorBar, LinearColorMapper, PrintfTickFormatter
-from bokeh.models.widgets import Slider, TextInput, Select
-import numpy as np
-from bokeh.plotting import figure, show
-import dataiku
+
 import pandas as pd
 
-# Parameterize webapp inputs
-input_dataset = "person_taste_matrix"
-x_column = "person1"
-y_column = "person2"
-score_column = "score"
+from bokeh.models import BasicTicker, ColorBar, LinearColorMapper, PrintfTickFormatter
+from bokeh.plotting import figure, show
+from bokeh.sampledata.unemployment1948 import data
 
-# Set up data
-mydataset = dataiku.Dataset(input_dataset)
-df = mydataset.get_dataframe()
+data['Year'] = data['Year'].astype(str)
+data = data.set_index('Year')
+data.drop('Annual', axis=1, inplace=True)
+data.columns.name = 'Month'
+
+years = list(data.index)
+months = list(data.columns)
+
+# reshape to 1D array or rates with a month and year for each row.
+df = pd.DataFrame(data.stack(), columns=['rate']).reset_index()
 
 # this is the colormap from the original NYTimes plot
 colors = ["#75968f", "#a5bab7", "#c9d9d3", "#e2e2e2", "#dfccce", "#ddb7b1", "#cc7878", "#933b41", "#550b1d"]
-mapper = LinearColorMapper(palette=colors, low=df.score.min(), high=df.score.max())
+mapper = LinearColorMapper(palette=colors, low=df.rate.min(), high=df.rate.max())
 
 TOOLS = "hover,save,pan,box_zoom,reset,wheel_zoom"
 
-p = figure(title="Wine tasting similarity heatmap",
-           x_range=x_column, y_range=y_column,
+p = figure(title="US Unemployment ({0} - {1})".format(years[0], years[-1]),
+           x_range=years, y_range=list(reversed(months)),
            x_axis_location="above", width=900, height=400,
-           tools=TOOLS, toolbar_location='below')
+           tools=TOOLS, toolbar_location='below',
+           tooltips=[('date', '@Month @Year'), ('rate', '@rate%')])
 
 p.grid.grid_line_color = None
 p.axis.axis_line_color = None
@@ -36,9 +36,9 @@ p.axis.major_label_text_font_size = "7px"
 p.axis.major_label_standoff = 0
 p.xaxis.major_label_orientation = pi / 3
 
-p.rect(x="Person1", y="Person2", width=1, height=1,
+p.rect(x="Year", y="Month", width=1, height=1,
        source=df,
-       fill_color={'field': 'score', 'transform': mapper},
+       fill_color={'field': 'rate', 'transform': mapper},
        line_color=None)
 
 color_bar = ColorBar(color_mapper=mapper, major_label_text_font_size="7px",
