715725710fa950b33321693193e583df92ed7f30
admin <nicolas.rouyer@neo4j.com>
2022-04-28 21:10:39 +0000

    Saved web app the wine app (TRYDSS.2rNFvJz)

--- web_apps/2rNFvJz/backend.py
+++ web_apps/2rNFvJz/backend.py
@@ -7,8 +7,27 @@ import dataiku
 import pandas as pd
 
 # Parameterize webapp inputs
-input_dataset = "Orders_enriched_prepared"
-x_column = "age"
-y_column = "total"
-time_column = "order_date_year"
-cat_column = "tshirt_category"
+input_dataset = "person_taste_matrix"
+x_column = "person1"
+y_column = "person2"
+score_column = "score"
+
+# Set up data
+mydataset = dataiku.Dataset(input_dataset)
+df = mydataset.get_dataframe()
+
+x = df[x_column]
+y = df[y_column]
+source = ColumnDataSource(data=dict(x=x, y=y))
+
+# Set up plot
+plot = figure(plot_height=400, plot_width=400, title=y_column+" by "+x_column,
+              tools="crosshair,pan,reset,save,wheel_zoom",
+              x_range=[min(x), max(x)], y_range=[min(y),max(y)])
+
+plot.scatter('x', 'y', source=source)
+
+# Set up layouts and add to document
+inputs = widgetbox()
+
+curdoc().add_root(row(inputs, plot, width=800))
