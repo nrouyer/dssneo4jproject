5316e056fc47a042228e3577d910cd2c34fa619d
admin <nicolas.rouyer@neo4j.com>
2022-04-28 20:57:18 +0000

    Saved web app the wine app (TRYDSS.2rNFvJz)

--- web_apps/2rNFvJz/backend.py
+++ web_apps/2rNFvJz/backend.py
@@ -1,1 +1,15 @@
 from bokeh.io import curdoc
+from bokeh.layouts import row, widgetbox
+from bokeh.models import ColumnDataSource
+from bokeh.models.widgets import Slider, TextInput, Select
+from bokeh.plotting import figure
+import dataiku
+import pandas as pd
+
+# Parameterize webapp inputs
+input_dataset = "Orders_enriched_prepared"
+x_column = "age"
+y_column = "total"
+time_column = "order_date_year"
+cat_column = "tshirt_category"
+
