741f9d216f361cd92fb4ddd13958c3d21b27ac5a
admin <nicolas.rouyer@neo4j.com>
2022-04-28 22:34:42 +0000

    Saved web app app 1 (TRYDSS.EEVYrqx)

--- web_apps/EEVYrqx/backend.py
+++ web_apps/EEVYrqx/backend.py
@@ -1,9 +1,12 @@
 import dataiku
-from bokeh.io import curdoc
+from bokeh.io import curdoc, output_file, show
 from bokeh.plotting import figure
 import pandas
 from pandas import DataFrame
-
+from bokeh.io import 
+from bokeh.models import (BasicTicker, ColorBar, ColumnDataSource,
+                          LinearColorMapper, PrintfTickFormatter)
+from bokeh.transform import transform
 
 # Uncomment the following to read your own dataset
 dataset = dataiku.Dataset("person_taste_matrix")
