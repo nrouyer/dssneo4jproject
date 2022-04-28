9700ddcea732dacfa7b7db4b11d28196df0aec23
admin <nicolas.rouyer@neo4j.com>
2022-04-28 21:50:08 +0000

    Saved web app the wine app (TRYDSS.2rNFvJz)

--- web_apps/2rNFvJz/backend.py
+++ web_apps/2rNFvJz/backend.py
@@ -1,12 +1,10 @@
+from math import pi
 from bokeh.io import curdoc
 from bokeh.layouts import row, widgetbox
-from bokeh.models import ColumnDataSource
+from bokeh.models import BasicTicker, ColorBar, LinearColorMapper, PrintfTickFormatter
 from bokeh.models.widgets import Slider, TextInput, Select
-from bokeh.plotting import figure
 import numpy as np
-import holoviews as hv
-import hvplot.pandas
-hv.extension('bokeh')
+from bokeh.plotting import figure, show
 import dataiku
 import pandas as pd
 
@@ -20,4 +18,33 @@ score_column = "score"
 mydataset = dataiku.Dataset(input_dataset)
 df = mydataset.get_dataframe()
 
-df.hvplot.heatmap(x='person1', y='person2', colorbar=True)
+# this is the colormap from the original NYTimes plot
+colors = ["#75968f", "#a5bab7", "#c9d9d3", "#e2e2e2", "#dfccce", "#ddb7b1", "#cc7878", "#933b41", "#550b1d"]
+mapper = LinearColorMapper(palette=colors, low=df.rate.min(), high=df.rate.max())
+
+TOOLS = "hover,save,pan,box_zoom,reset,wheel_zoom"
+
+p = figure(title="Wine tasting similarity heatmap",
+           x_range=person1, y_range=person2,
+           x_axis_location="above", width=900, height=400,
+           tools=TOOLS, toolbar_location='below')
+
+p.grid.grid_line_color = None
+p.axis.axis_line_color = None
+p.axis.major_tick_line_color = None
+p.axis.major_label_text_font_size = "7px"
+p.axis.major_label_standoff = 0
+p.xaxis.major_label_orientation = pi / 3
+
+p.rect(x="Person1", y="Person2", width=1, height=1,
+       source=df,
+       fill_color={'field': 'score', 'transform': mapper},
+       line_color=None)
+
+color_bar = ColorBar(color_mapper=mapper, major_label_text_font_size="7px",
+                     ticker=BasicTicker(desired_num_ticks=len(colors)),
+                     formatter=PrintfTickFormatter(format="%d%%"),
+                     label_standoff=6, border_line_color=None)
+p.add_layout(color_bar, 'right')
+
+show(p)
