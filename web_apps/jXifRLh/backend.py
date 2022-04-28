aa52b92ae2efca183c9d877a456c6e8c7e78dcdc
admin <nicolas.rouyer@neo4j.com>
2022-04-28 22:13:28 +0000

    Saved web app sample webapp (TRYDSS.jXifRLh)

--- web_apps/jXifRLh/backend.py
+++ web_apps/jXifRLh/backend.py
@@ -1,6 +1,5 @@
 from bokeh.plotting import figure, show, output_file
 
-output_file('image.html')
 
 p = figure(x_range=(0, 1), y_range=(0, 1))
 p.image_url(url=['/local/static/wine_tasting.jpg'], x=0, y=1, w=0.8, h=0.6)
