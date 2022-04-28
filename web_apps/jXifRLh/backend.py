b0ccba45b03ebd705dd41fa6998dc7194110dc92
admin <nicolas.rouyer@neo4j.com>
2022-04-28 22:09:04 +0000

    Saved web app sample webapp (TRYDSS.jXifRLh)

--- web_apps/jXifRLh/backend.py
+++ web_apps/jXifRLh/backend.py
@@ -1,5 +1,10 @@
 from bokeh.io import curdoc
+from bokeh.plotting import figure, show, output_file
 
+output_file('image.html')
 path = "/local/static/wine_tasting.jpg"
+p = figure(x_range=(0, 1), y_range=(0, 1))
+p.image_url(url=[path], x=0, y=1, w=0.8, h=0.6)
 
-p.image_url(url=[path])
+show(p)
+
