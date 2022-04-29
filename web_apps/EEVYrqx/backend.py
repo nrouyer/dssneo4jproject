6ddd39bcc30d218eeb8605159b99b5fea893f878
admin <nicolas.rouyer@neo4j.com>
2022-04-29 09:30:41 +0000

    Saved web app app 1 (TRYDSS.EEVYrqx)

--- web_apps/EEVYrqx/backend.py
+++ web_apps/EEVYrqx/backend.py
@@ -1,26 +1,49 @@
 import dataiku
 from bokeh.io import curdoc, output_file, show
-from bokeh.plotting import figure
-import pandas
-from pandas import DataFrame
+import pandas as pd
+
+from bokeh.io import output_file, show
 from bokeh.models import (BasicTicker, ColorBar, ColumnDataSource,
                           LinearColorMapper, PrintfTickFormatter)
+from bokeh.plotting import figure
+from bokeh.sampledata.unemployment1948 import data
 from bokeh.transform import transform
 
-# Uncomment the following to read your own dataset
-dataset = dataiku.Dataset("person_taste_matrix")
-df = dataset.get_dataframe()
-column = "score"
+output_file("unemploymemt.html")
 
-from bokeh.plotting import figure, show
+data.Year = data.Year.astype(str)
+data = data.set_index('Year')
+data.drop('Annual', axis=1, inplace=True)
+data.columns.name = 'Month'
 
-colors = ['#2765a3' for x in range(20)]
-x = list(range(10)) * 2
-y = ['a'] * 10 +  ['b'] * 10
+# reshape to 1D array or rates with a month and year for each row.
+df = pd.DataFrame(data.stack(), columns=['rate']).reset_index()
 
-hm = figure(y_range=('a', 'b'))
-hm.rect(x, y, width=1, height=1, fill_color=colors, line_color="white")
+source = ColumnDataSource(df)
 
-show(hm)
+# this is the colormap from the original NYTimes plot
+colors = ["#75968f", "#a5bab7", "#c9d9d3", "#e2e2e2", "#dfccce", "#ddb7b1", "#cc7878", "#933b41", "#550b1d"]
+mapper = LinearColorMapper(palette=colors, low=df.rate.min(), high=df.rate.max())
 
-curdoc().add_root(hm)
+p = figure(width=800, height=300, title="US unemployment 1948—2016",
+           x_range=list(data.index), y_range=list(reversed(data.columns)),
+           toolbar_location=None, tools="", x_axis_location="above")
+
+p.rect(x="Year", y="Month", width=1, height=1, source=source,
+       line_color=None, fill_color=transform('rate', mapper))
+
+color_bar = ColorBar(color_mapper=mapper,
+                     ticker=BasicTicker(desired_num_ticks=len(colors)),
+                     formatter=PrintfTickFormatter(format="%d%%"))
+
+p.add_layout(color_bar, 'right')
+
+p.axis.axis_line_color = None
+p.axis.major_tick_line_color = None
+p.axis.major_label_text_font_size = "7px"
+p.axis.major_label_standoff = 0
+p.xaxis.major_label_orientation = 1.0
+
+show(p)
+
+curdoc().add_root(p)
