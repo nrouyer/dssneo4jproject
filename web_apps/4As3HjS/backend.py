59306bd8a88721c47aef83d6fd5e81bd079b4fd7
admin <nicolas.rouyer@neo4j.com>
2022-04-29 10:27:21 +0000

    Saved web app wine app bokeh (TRYDSS.4As3HjS)

--- web_apps/4As3HjS/backend.py
+++ web_apps/4As3HjS/backend.py
@@ -65,7 +65,7 @@ p1 = figure(width=800, height=300, title="Wine tasting similarity",
 p1.rect(x="person1", y="person2", width=1, height=1, source=source1,
        line_color=None, fill_color=transform('score', mapper1))
 
-color_bar1 = ColorBar(color_mapper=mapper,
+color_bar1 = ColorBar(color_mapper=mapper1,
                      ticker=BasicTicker(desired_num_ticks=len(colors)),
                      formatter=PrintfTickFormatter(format="%d%%"))
 
