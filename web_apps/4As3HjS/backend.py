b7eb56326e1f95973e7a90c0d1eb71eba4c2e6c3
admin <nicolas.rouyer@neo4j.com>
2022-04-29 10:03:54 +0000

    Saved web app wine app bokeh (TRYDSS.4As3HjS)

--- web_apps/4As3HjS/backend.py
+++ web_apps/4As3HjS/backend.py
@@ -44,8 +44,39 @@ p.axis.major_label_text_font_size = "7px"
 p.axis.major_label_standoff = 0
 p.xaxis.major_label_orientation = 1.0
 
+input_dataset = ""
+x_column = "person1"
+y_column = "person2"
+score_column = "score"
 
+# Set up data
+mydataset = dataiku.Dataset(input_dataset)
+df1 = mydataset.get_dataframe()
+source1 = ColumnDataSource(df1)
+
+# this is the colormap from the original NYTimes plot
+colors1 = ["#75968f", "#a5bab7", "#c9d9d3", "#e2e2e2", "#dfccce", "#ddb7b1", "#cc7878", "#933b41", "#550b1d"]
+mapper1 = LinearColorMapper(palette=colors, low=df1.score.min(), high=df1.score.max())
+
+p1 = figure(width=800, height=300, title="Wine tasting similarity",
+           x_range=list(df1.person1), y_range=list(df1.person2),
+           toolbar_location=None, tools="", x_axis_location="above")
+
+p1.rect(x="person1", y="person2", width=1, height=1, source=source1,
+       line_color=None, fill_color=transform('score', mapper))
+
+color_bar1 = ColorBar(color_mapper=mapper,
+                     ticker=BasicTicker(desired_num_ticks=len(colors)),
+                     formatter=PrintfTickFormatter(format="%d%%"))
+
+p1.add_layout(color_bar1, 'right')
+
+p1.axis.axis_line_color = None
+p1.axis.major_tick_line_color = None
+p1.axis.major_label_text_font_size = "7px"
+p1.axis.major_label_standoff = 0
+p1.xaxis.major_label_orientation = 1.0
 
 #show(p)
 
-curdoc().add_root(p)
+curdoc().add_root(p1)
