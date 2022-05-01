017b988035f132bc962228f52fbae43767b986dc
admin <nicolas.rouyer@neo4j.com>
2022-05-01 20:37:56 +0000

    Saved web app wine app bokeh (TRYDSS.4As3HjS)

--- web_apps/4As3HjS/backend.py
+++ web_apps/4As3HjS/backend.py
@@ -103,5 +103,7 @@ for w in [min_score, max_score]:
 inputs = column(min_score, max_score)
     
 curdoc().add_root(p1)
-curdoc().add_root(min_score)
-curdoc().add_root(max_score)
+#curdoc().add_root(min_score)
+#curdoc().add_root(max_score)
+show(min_score)
+show (max_score)
