0a899665fe4cbd0ba6c516bf60e1b61ad3069dbf
admin <nicolas.rouyer@neo4j.com>
2022-05-02 22:20:16 +0000

    Saved web app wine app bokeh (TRYDSS.4As3HjS)

--- web_apps/4As3HjS/backend.py
+++ web_apps/4As3HjS/backend.py
@@ -95,7 +95,7 @@ def update_data(attrname, old, new):
     #x = selected.person1
     #y = selected.person2
     #source1.data = dict(x=x, y=y, score=score)
-    source2 = ColumnDataSource(selected)
+    source1 = ColumnDataSource(selected)
     #source1.data = new1.data
     #source1.data = dict(x=x, y=y)
     #source1.change.emit()
@@ -103,7 +103,7 @@ def update_data(attrname, old, new):
            x_range=df1['person1'].unique(), y_range=df1['person2'].unique(),
            toolbar_location=None, tools="", x_axis_location="above")
 
-    p1.rect(x="person1", y="person2", width=1, height=1, source=source2,
+    p1.rect(x="person1", y="person2", width=1, height=1, source=source1,
        line_color=None, fill_color=transform('score', mapper1))
     
 for w in [min_score, max_score]:
